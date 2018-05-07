82fea17cb45d894f63c55b8c571ee12dcf5f5b11
unknown <=>
2018-05-06 23:15:28 -0500

    Added new explore lines.

--- 1-explore.R
+++ 1-explore.R
@@ -1,7 +1,21 @@
 # Assumes you've run 0-read.R.
 
 # Explore one account.
-owns = own %>% filter( `Account Number` == 1554 )
-lics = lic %>% filter( `ACCOUNT NUMBER` == 1554 ) %>% arrange( `LICENSE TERM START DATE` )
-View(owns)
-View(lic)
+
+  ian = 1554  
+
+  # Owners
+  owners = own %>% filter( `Account Number` == ian )
+  
+  # Licenses. 
+  # We may need to get license numbers first in case licenense change accounts. I'm not sure if this is the case.
+  # Either way, we will need it for inspections.
+  license_numbers = lic$`LICENSE NUMBER`[ lic$`ACCOUNT NUMBER` == ian ] %>% unique()
+  licenses = lic %>% filter( `LICENSE NUMBER` %in% license_numbers ) %>% arrange( `LICENSE TERM START DATE` )
+  
+  # Inspections.
+  inspections = food %>% filter( `License #` %in% license_numbers ) %>% arrange(`Inspection Date`)
+  
+  View(owners)
+  View(licenses)
+  View(inspections)
